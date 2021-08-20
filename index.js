2a3a34f9414a29f4b9b20dacb6532e4cc03bd82c
andrewhunt21 <andrewhunt21@icloud.com>
2021-08-20 09:31:46 -0700

    finished tasks 1 & 2

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ myFunction();
 
 //🚀🚀🚀 ⬇️ 📝 Explanation ⬇️ 📝 🚀🚀🚀: 
 
-
+// because internal is nested inside myFunction and once myFunction is invoked the console log inside my function executes.
 
 
 
@@ -28,11 +28,10 @@ myFunction();
     
 For example, `summation(4)` should return 10 because 1+2+3+4 is 10. Note, you may use a for loop for this function if you wish */
 
-function summation(/*Your Code Here*/) {
-  /*Your Code Here*/
-
-  }
- 
+function summation(num) {
+  return (num*(num+1))/2;
+}
+ console.log('task 2', summation(4));
 
 // 🦁🦁🦁 Topic 2: ADVANCED Array Methods 🦁🦁🦁
 // Given this zoo data from around the United States, follow the instructions below. Use the specific array methods in the requests below to solve the problems.
